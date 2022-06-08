c0c91d0c05c9b00dc21b55831547cb4e108f2bb6
Giovanni Orsi <silvestre1602@gmail.com>
2022-06-08 16:59:54 -0400

    fix category routes controller

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,10 +1,9 @@
 const connection = require('../db_connection');
 
-// Get all categories 
+// Get all categories
 module.exports.getAll = function(req, res){
     connection.query('SELECT * FROM category', function (error, results, fields) {
-        if (error) throw error;
-        console.log(results);
-        res.send(results);
+        if (error) return res.send({status: 'error', message:error});
+        return res.send({status: 'success', data: JSON.parse( JSON.stringify(results))});
     });
 }
